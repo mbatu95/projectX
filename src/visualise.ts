e50f86e82961a1d59b1f2163d1c674945bf4b5df
mbatu95 <mbatu95@outlook.com>
2025-03-01 16:31:52 +0700

    start

--- src/visualise.ts
+++ src/visualise.ts
@@ -1,15 +1,15 @@
-// import * as fs from 'fs';
+const fs = require('fs');
 
 // // Specify the path to your file
-// const filePath = 'src\inputs\shirt_stitch_contours.json'; // Change this to the actual path of your file
+const filePath = 'src/inputs/shirt_stitch_contours.json'; // Change this to the actual path of your file
 
-// // Use fs.readFile to read the file
-// fs.readFile(filePath, 'utf8', (err, data) => {
-//   if (err) {
-//     console.error('Error reading file:', err);
-//     return;
-//   }
-//   // Now data is in scope and can be used here
-//   console.log(data);
-// });
+// Use fs.readFile to read the file
+fs.readFile(filePath, 'utf8', (err: any, data: any) => {
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
+  // Now data is in scope and can be used here
+  console.log(data);
+});
 console.log("batu");
